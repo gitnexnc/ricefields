f8adc87ab15b6e33a3a5a98f95ac266056497b50
NEXNC <git@nexnc.com>
2026-02-27 17:21:50 +0530

    fixed package name error during home manager evaluation

--- home/modules/programs/gpg.nix
+++ home/modules/programs/gpg.nix
@@ -14,7 +14,7 @@
   services.gpg-agent = {
     enable = true;
 
-    pinentryPackage = pkgs.pinentry-curses;
+    pinentry.package = pkgs.pinentry-curses;
 
     # Cache the passphrase for 8 hours (28800 seconds)
     # This prevents having to re-type it for every commit
