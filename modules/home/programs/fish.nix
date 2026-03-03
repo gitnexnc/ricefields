37efcd04e7377ecfe2b93cee469f61f5bcb87cb2
NEXNC <git@nexnc.com>
2026-03-03 17:16:35 +0530

    feat(fish): add gpg-agent TTY and SSH socket init

--- modules/home/programs/fish.nix
+++ modules/home/programs/fish.nix
@@ -11,6 +11,7 @@ in
       set -g fish_greeting ""
       set -gx PATH $HOME/.cargo/bin $PATH
       set -gx GPG_TTY (tty)
+      gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
       set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     '';
 
